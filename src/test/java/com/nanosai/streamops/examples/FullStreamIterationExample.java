7c0ba1b7ff59e433b8ff43cfe76290563ed608c9
Jakob Jenkov <jakob@jenkov.com>
2019-08-19 18:15:17 +0200

    minor change to record structure

--- src/test/java/com/nanosai/streamops/examples/FullStreamIterationExample.java
+++ src/test/java/com/nanosai/streamops/examples/FullStreamIterationExample.java
@@ -9,6 +9,7 @@ import com.nanosai.streamops.util.FileUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.GregorianCalendar;
 
 public class FullStreamIterationExample {
 
@@ -22,10 +23,18 @@ public class FullStreamIterationExample {
                 .setNestedFieldStack(new int[16])
                 .setDestination(rionRecord,0);
 
+        //create an order item record
         rionWriter.writeObjectBeginPush(2);
-        rionWriter.writeUtf8("Value1");
-        rionWriter.writeUtf8("Value2");
-        //rionWriter.writeUtf8("Value3");
+        rionWriter.writeInt64( 987654321);  //orderItemId
+        rionWriter.writeInt64(99999);      //productId
+        rionWriter.writeInt64(9999);      //orderId
+        rionWriter.writeInt64( 123);      //customerId
+        rionWriter.writeInt64( 99);       //price - integer part (e.g. whole dollars)
+        rionWriter.writeInt64(95);        //price - fraction part (e.g. cents)
+        rionWriter.writeInt64( 69);       //cost - integer part (e.g. whole dollars)
+        rionWriter.writeInt64(45);        //cost - fraction part (e.g. cents)
+        GregorianCalendar dateTime = new GregorianCalendar();
+        rionWriter.writeUtc( dateTime, 9);  //date-time in UTC, 9 bytes = including milliseconds
         rionWriter.writeObjectEndPop();
 
         //byte[] rionBytesRecord1 = new byte[]{0x01, 0x08, 0,1,2,3,4,5,6,7}; //10 bytes in total, 8 bytes in the RION Bytes field body
@@ -57,7 +66,11 @@ public class FullStreamIterationExample {
                     rionReader.moveInto();
                     while(rionReader.hasNext()){
                         rionReader.nextParse();
-                        System.out.println("   [" + recordOffset + "][" + rionReader.fieldType +"]["+ rionReader.readUtf8String() +"]");
+                        if(rionReader.fieldType == RionFieldTypes.UTF_8) {
+                            System.out.println("   [" + recordOffset + "][" + rionReader.fieldType + "][" + rionReader.readUtf8String() + "]");
+                        } else if (rionReader.fieldType == RionFieldTypes.INT_POS){
+                            System.out.println("   [" + recordOffset + "][" + rionReader.fieldType + "][" + rionReader.readInt64() + "]");
+                        }
                     }
                     rionReader.moveOutOf();
 
